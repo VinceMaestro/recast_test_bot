291878aa57dd07699253fe8753d2bb4d22a2e6d7
VinceMaestro <vincent.petit.danjou@gmail.com>
2017-06-16 18:04:48 +0200

    fix

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {}
+
+config.pageAccessToken = 'EAABZBYpFSq18BAEjXxGVsUzrbpQ8p80KwtlwZBrBsvZChUTVxqlxta4Br8b7tv1wsfmAmZAf9mof2KmvbYDFaYcEpHoq0hSA8XEN7uagHZAS7cGhbf0dNqDHRodFDjRkHJHJVzesfpu5hKNgzSTjF9Do62wHOz4PZAnKqZBOF497tVR2o3voedE'
+config.validationToken = 'ABCD1234'
+config.recastToken = '39fcababfbb02d0cf7ec4d8922e16f7e'
+config.language = 'fr'
+
+module.exports = config
